4bb28305124485b091dcbc6c9b396ae26d8be6b6
Samridhi Kansal <kansal.samridhi@gmail.com>
2020-09-12 09:24:46 +0530

    heart on image

--- src/components/collection-item/collection-item.js
+++ src/components/collection-item/collection-item.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./collection-item.styles.scss";
+import { FaHeart } from "react-icons/fa";
 
 const CollectionItem = ({ id, name, price, imageUrl }) => {
   return (
@@ -9,12 +10,19 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
         style={{
           backgroundImage: `url(${imageUrl})`,
         }}
-      />
+      >
+        {" "}
+        <FaHeart color="red" size="20px">
+          {" "}
+        </FaHeart>{" "}
+      </div>
+
       <div className="collection-footer">
         <span className="name">{name}</span>
 
         <span className="price">{price}</span>
       </div>
+      <button> Add to Cart</button>
     </div>
   );
 };
