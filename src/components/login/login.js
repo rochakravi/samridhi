952ff4aed79d66725a4ad7875a386463b0d659e9
ravi <rochak.ravi@gmail.com>
2020-09-20 16:02:43 +0530

    clean up

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,47 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { connect } from "react-redux";
 
 export class login extends Component {
-    constructor(props) {
-        super(props);
-        this.login = this.login.bind(this);
-    }
-    login = () => {
-        let item = {
-            isAuth: true,
-            user: [{
-                "id": 1,
-                "name": 'Achal'
-            }]
-        }
-        debugger;
-        this.props.login(item);
-        if (this.props.user) {
-            alert("Successfully : " + this.props.user);
-        }
-        else {
-            {
-                alert("Error : " + this.props.user);
-            }
-        }
-    };
-    render() {
-        return (
-            <div>
-                <input type="button" value="Login" onClick={this.login}></input>
-            </div>
-        )
-    }
+  constructor() {
+    super();
+  }
+
+  test = () => {
+    alert("Successfully : " + this.props.user);
+    alert("Successfully : " + this.props.name);
+  };
+  render() {
+    return (
+      <div>
+        <input type="button" value="Login" onClick={this.props.login}></input>
+        <input type="button" value="Test" onClick={this.test}></input>
+      </div>
+    );
+  }
 }
 const mapStateToProps = (state) => {
-    return { user: state.user.isAuth };
+  return { user: state.user.isAuth, name: state.user.name };
 };
 const mapDispatchtoProps = (dispatch) => {
-    return {
-        login: (item) =>
-            dispatch({
-                type: "LOGIN", payload: item
-            }),
-    };
-}
-export default connect(mapStateToProps, mapDispatchtoProps)(login)
+  return {
+    login: (item) =>
+      dispatch({
+        type: "LOGIN",
+      }),
+  };
+};
+export default connect(mapStateToProps, mapDispatchtoProps)(login);
--- src/store/reducers/userReducers.js
+++ src/store/reducers/userReducers.js
@@ -1,29 +1,30 @@
-import LOGIN from '../action/index'
-import LOGOUT from '../action/index'
+import LOGIN from "../action/index";
+import LOGOUT from "../action/index";
 
 const iniialState = {
-    isAuth: false,
-    user: []
+  isAuth: false,
+  name: "name",
+  user: [],
 };
 const UserReducers = (state = iniialState, action) => {
-    debugger;
-    switch (action.type) {
-        case "LOGIN":
-            return {
-                ...state,
-                isAuth: true,
-                user: state.user.concat(action.payload),
-            }
-        case "LOGOUT":
-            return {
-                ...state,
-                isAuth: true,
-                user: state.user.concat(action.payload),
-            }
-        default:
-            return state;
-    }
-    return state;
-}
+  switch (action.type) {
+    case "LOGIN":
+      return {
+        ...state,
+        isAuth: true,
+        name: "ravi",
+        // user: state.user.concat(action.payload),
+      };
+    case "LOGOUT":
+      return {
+        ...state,
+        isAuth: true,
+        user: state.user.concat(action.payload),
+      };
+    default:
+      return state;
+  }
+  return state;
+};
 
 export default UserReducers;
